f718a996e3263cf84ed84c9515cb86464f55ad9b
YunKieun <rena0816@naver.com>
2023-04-19 18:15:08 +0900

    sql

--- mydb/board.sql
+++ mydb/board.sql
@@ -6,6 +6,29 @@ CREATE TABLE board(
     content VARCHAR2(2000) NOT NULL,   -- 글내용
     regdate DATE DEFAULT SYSDATE       -- 등록일
 );
+
+SELECT ROWNUM, bno, title, content
+FROM board 
+WHERE ROWNUM > 0 AND ROWNUM <= 10;
+-- WHERE ROWNUM > 11 AND ROWNUM <= 20; -- ROWNUM은 1을 포합해야함
+
+-- 페이지 처리
+SELECT * 
+FROM
+ (SELECT ROWNUM rn, bno, title, content
+  FROM board) 
+WHERE rn >= 11 AND rn <= 20; -- ROWNUM의 별칭을 사용하면 가능함 
+
+-- ROWID
+-- 데이터를 구분하는 유일한 값
+-- ROWID를 통해서 데이터 파일, 어느 블럭에 저장되어 있는지 알 수 있음
+SELECT ROWID, bno, title, content
+FROM board;
+
+SELECT ROWID, bno, title, content
+FROM board
+WHERE ROWID = 'AAAS1lAAHAAAAG9AAA';
+
 -- 시퀀스(일련번호, 자동순번)
 CREATE SEQUENCE seq;
 DROP SEQUENCE seq;
@@ -31,8 +54,8 @@ values(seq.NEXTVAL, '좋은 하루', '긴하루', '좋은 하루 되세요~');
 
 
 -- 게시글 검색
-SELECT *
-FROM board;
+SELECT * FROM board
+ORDER BY regdate DESC;
 
 -- 작성자가 관리자인 게시글을 검색하시오.
 SELECT * FROM board
@@ -46,5 +69,10 @@ WHERE bno = 2;
 -- 3번 게시글을 삭제하시오.
 -- DELETE FROM 테이블이름 WHERE 절
 DELETE FROM board WHERE bno = 3;
+
+-- 재귀 복사 (자료 삽입)
+-- INSERT INTO(칼럼) (SELECT 칼럼 FROM 테이블이름)
+INSERT INTO board(bno, title, writer, content)
+(SELECT seq.nextval, title, writer, content FROM board);
 
 ROLLBACK;
